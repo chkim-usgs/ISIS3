a9b311786f113c640e237b9cf0ce4b4e3a17f285
Christine Kim <chkim@usgs.gov>
2025-01-14 09:47:11 -0500

    test

--- .github/pr_label_checker.py
+++ .github/pr_label_checker.py
@@ -45,7 +45,7 @@ def get_pr_attributes(response: Response) -> tuple:
     print(str(pull_response_json))
     if not len(pull_response_json):
         # No PRs attributed to the commit
-        print(False + "HELLO, response: " + str(pull_response_json))
+        print(False + "length=" + len(pull_response_json) + ", HELLO, response: " + str(pull_response_json) + pull_response_json)
         sys.exit(0)
     pull_number = pull_response_json[0].get("number")
     pull_body = pull_response_json[0].get("body")
